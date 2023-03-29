0f65e04f0a4b8d16f579424c317713ec7e3051c1
zhangq-office <zhangq@yunrecy.com>
2023-03-29 10:58:41 +0800

    修改

---
+++ lib/ec-canvas/ce.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>33332342</div>
+</template>
+
+<script setup>
+import { EChart } from '../../pck/index.es.js'
+console.log(EChart, 'echarts')
+</script>
+
+<style scoped></style>
